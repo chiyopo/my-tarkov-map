955a3219d1d85b56058d3dea006c4df45532e213
chiyopo <chiyo_1026@au.com>
2026-09-14 23:59:52 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -3946,7 +3946,6 @@ async function loadFactoryFloors() {
 async function loadLabsFloors() {
 
     const floorMaps = document.querySelectorAll(".labs-floor-map");
-console.log("Labs floor maps:", floorMaps.length);
 
     try {
         const response = await fetch("Labs.svg");
